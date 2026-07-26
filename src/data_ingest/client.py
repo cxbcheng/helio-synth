a162e445c8a4e1f9feab17508ff84688dd0389c6
cxbcheng <christian.b.cheng@gmail.com>
2026-07-26 03:02:18 -0400

    Started export request method for extracting velocity time series; WIP

--- src/data_ingest/client.py
+++ src/data_ingest/client.py
@@ -1,5 +1,6 @@
 import os
 import drms
+import numpy as np
 from dotenv import load_dotenv
 
 def get_drms_client(email: str | None = None) -> drms.Client:
@@ -14,3 +15,48 @@ def get_drms_client(email: str | None = None) -> drms.Client:
 
     client = drms.Client(email=email)
     return client
+
+def extract_velocity_timeseries(
+    start_time: str,
+    end_time: str,
+    x: float,
+    y: float,
+    download_dir: str,
+    email: str | None = None,
+) -> np.ndarray:
+    """
+    TODO: documentation
+    :param start_time:
+    :param end_time:
+    :param x:
+    :param y:
+    :param download_dir:
+    :param email:
+    :return:
+    """
+    client = get_drms_client(email)
+    qstr = f"hmi.v_45s[{start_time}-{end_time}]"
+
+    print(f"Submitting export request for {qstr} at ({x}, {y})...")
+    # query = client.query(qstr, key="OBS_VR")
+    # print(query.head)
+
+    # FIXME: added record limit n = 10
+    export_request = client.export(qstr, protocol='fits', n=10)
+    print("Waiting for JSOC servers...")
+    print(export_request.request_url)
+    export_request.wait()
+
+    print(export_request.data)
+
+
+# FIXME: Remove tests
+# Note how long these export requests take! Make sure to implement cutouts so that we don't try to
+# extract the entire 4096x4096 image, even over the course of a day (which is about 3840 images).
+extract_velocity_timeseries(
+    start_time='2020-01-01T00:00:00Z',
+    end_time='2020-01-01T00:59:59Z',
+    x=0,
+    y=0,
+    download_dir='./',
+)
