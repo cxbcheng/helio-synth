e9d5dbbae8cfcbc77ffdf73027003fbd3c3a9f08
cxbcheng <christian.b.cheng@gmail.com>
2026-07-28 19:45:30 -0400

    added documentation for script

--- scripts/plot_raw_velocity_timeseries.py
+++ scripts/plot_raw_velocity_timeseries.py
@@ -1,19 +1,30 @@
-from astropy.visualization import time_support
+"""
+Plots a raw Doppler velocity time series from HMI observations.
+
+By default, this script uses the cached dataset for
+2020-01-01 to 2020-01-08 at the center of the solar disk
+(x=256, y=256). If the required FITS files are not present,
+you will be prompted to download them.
+"""
+
 from matplotlib import pyplot as plt
-from matplotlib.ticker import MultipleLocator
 
 from src.data_ingest.ingest import run_ingest
 
-# TODO: documentation and prompts
-times, velocities = run_ingest()
-elapsed = (times - times[0]).sec
+# Change parameters here to test different regions/timeframes
+start_time = "2020.01.01_00:00:00_TAI"
+end_time = "2020.01.08_00:00:00_TAI"
+x = 256
+y = 256
 
-time_support(format='iso', simplify=True)
+# Load velocity time series
+times, velocities = run_ingest(start_time=start_time, end_time=end_time, x=x, y=y)
+elapsed = (times - times[0]).sec
 
 plt.figure()
 plt.plot(elapsed, velocities)
-plt.xlabel('Time since first 2020-01-01 00:00:00 [s]')
-plt.ylabel('Velocity [m/s]')
+plt.xlabel('Time since 2020-01-01 00:00:00 [s]')
+plt.ylabel('Doppler Velocity [m/s]')
 plt.title('Velocity vs Time at the Center of the Solar Disk')
 plt.grid(True)
 plt.show()
