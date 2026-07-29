ec2371f04444cfe38b359e2bbee73311695733c5
cxbcheng <christian.b.cheng@gmail.com>
2026-07-29 03:34:47 -0400

    encapsulated scripts in a main method

--- scripts/plot_signal_bandpass.py
+++ scripts/plot_signal_bandpass.py
@@ -19,32 +19,38 @@ from src.data_ingest.ingest import run_ingest
 from src.processing.cleaning import interpolate_short_gaps, zero_fill_with_taper
 from src.processing.filter import filter_segments
 
-# Change parameters here to test different regions/timeframes
-start_time = "2020.01.01_00:00:00_TAI"
-end_time = "2020.01.08_00:00:00_TAI"
-x = 256
-y = 256
-# Bandpass filter parameters; 5-minute oscillations = ~3 mHz
-low_cutoff = 0.001  # 1 mHz
-high_cutoff = 0.005  # 5 mHz
 
-# Matplotlib plot parameters
-plt_x_start = 3840
-plt_x_end = plt_x_start + 3600  # 60 minute duration
+def main():
+    # Change parameters here to test different regions/timeframes
+    start_time = "2020.01.01_00:00:00_TAI"
+    end_time = "2020.01.08_00:00:00_TAI"
+    x = 256
+    y = 256
+    # Bandpass filter parameters; 5-minute oscillations = ~3 mHz
+    low_cutoff = 0.001  # 1 mHz
+    high_cutoff = 0.005  # 5 mHz
 
-# Load velocity time series
-times, velocities = run_ingest(start_time=start_time, end_time=end_time, x=x, y=y)
-elapsed = (times - times[0]).sec
+    # Matplotlib plot parameters
+    plt_x_start = 3840
+    plt_x_end = plt_x_start + 3600  # 60 minute duration
 
-velocities = interpolate_short_gaps(velocities, max_gap=3)
-velocities = filter_segments(velocities, low_cutoff=low_cutoff, high_cutoff=high_cutoff, filter_order=2)
-velocities = zero_fill_with_taper(velocities)
+    # Load velocity time series
+    times, velocities = run_ingest(start_time=start_time, end_time=end_time, x=x, y=y)
+    elapsed = (times - times[0]).sec
 
-plt.figure()
-plt.plot(elapsed, velocities)
-plt.xlabel('Time since 2020-01-01 00:00:00 [s]')
-plt.ylabel('Doppler Velocity [m/s]')
-plt.title('Filtered Signal at the Center of the Solar Disk')
-plt.xlim(plt_x_start, plt_x_end)
-plt.grid(True)
-plt.show()
+    velocities = interpolate_short_gaps(velocities, max_gap=3)
+    velocities = filter_segments(velocities, low_cutoff=low_cutoff, high_cutoff=high_cutoff, filter_order=2)
+    velocities = zero_fill_with_taper(velocities)
+
+    plt.figure()
+    plt.plot(elapsed, velocities)
+    plt.xlabel('Time since 2020-01-01 00:00:00 [s]')
+    plt.ylabel('Doppler Velocity [m/s]')
+    plt.title('Filtered Signal at the Center of the Solar Disk')
+    plt.xlim(plt_x_start, plt_x_end)
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
