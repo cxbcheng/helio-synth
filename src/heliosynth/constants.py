acb96aa96ade1aea930a8e4f0226f194f351fd43
cxbcheng <christian.b.cheng@gmail.com>
2026-08-05 10:15:25 -0400

    revised velocity bounds, according to stats

--- src/heliosynth/constants.py
+++ src/heliosynth/constants.py
@@ -1,12 +1,13 @@
 # Velocity bounds for colormaps with index `detrend_order`
+# Partially based on values from `examples/print_velocity_stats.py`
 V_MIN = {
     None: -3000,
     0: -3000,
-    2: -450
+    2: -500
 }
 
 V_MAX = {
     None: 3000,
     0: 3000,
-    2: 450
+    2: 500
 }
